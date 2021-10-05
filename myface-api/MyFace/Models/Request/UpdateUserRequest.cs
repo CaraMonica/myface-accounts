7a8a4e6109c151ee090f792d1451b1f7c8484223
caramonica <caramonica@hotmail.co.uk>
2021-10-05 13:38:34 +0100

    Added password to user request model.

--- myface-api/MyFace/Models/Request/UpdateUserRequest.cs
+++ myface-api/MyFace/Models/Request/UpdateUserRequest.cs
@@ -6,6 +6,7 @@
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public string ProfileImageUrl { get; set; }
         public string CoverImageUrl { get; set; }
     }
