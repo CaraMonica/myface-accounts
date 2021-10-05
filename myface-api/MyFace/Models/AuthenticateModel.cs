f796851b328a91b583d25f149aa6c30020c14a1e
caramonica <caramonica@hotmail.co.uk>
2021-10-05 13:29:58 +0100

    Renamed

---
+++ myface-api/MyFace/Models/AuthenticateModel.cs
@@ -0,0 +1,11 @@
+namespace MyFace.Models
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        
+        [Required]
+        public string Password { get; set;}
+    }
+}
